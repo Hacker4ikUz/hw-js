fbcf13e5a92dde70033d427f87f6d74bef396f8b
Саломов Сухроб <59762516+Hacker4ikUz@users.noreply.github.com>
2022-04-05 07:16:01 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 let offset = 0;
 let slider = document.querySelector('.offer__slide')
+let days = document.querySelector('#days')
+let hours = document.querySelector('#hours')
+let minutes = document.querySelector('#minutes')
+let seconds = document.querySelector('#seconds')
 
 document.querySelector('.offer__slider-next').addEventListener('click', function() {
     offset += 585;
@@ -16,3 +20,25 @@ document.querySelector('.theleft').addEventListener('click', function() {
     }
     slider.style.left = -offset + 'px';
 })
+
+
+const deadline = new Date(`2022-04-09`);
+
+function updateCounter() {
+  const currentTime = new Date();
+  const dist = deadline - currentTime;
+
+  const toDays = Math.floor(dist / 1000 / 60 / 60 / 24);
+  const toHours = Math.floor(dist / 1000 / 60 / 60) % 24;
+  const toMinutes = Math.floor(dist / 1000 / 60) % 60;
+  const toSeconds = Math.floor(dist / 1000) % 60;
+
+  days.innerText = toDays;
+  hours.innerText = toHours < 10 ? "0" + toHours : toHours;
+  minutes.innerText = toMinutes < 10 ? "0" + toMinutes : toMinutes;
+  seconds.innerText = toSeconds < 10 ? "0" + toSeconds : toSeconds;
+}
+
+updateCounter();
+
+setInterval(updateCounter, 1000);
